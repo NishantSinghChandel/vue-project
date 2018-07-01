a8258d04eccbc3c17b9c904e4830d012cc589186
Nishant <nishant@grappus.com>
2018-07-02 00:18:15 +0530

    A vuejs project setup

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Layout>
+      <router-view/>
+    </Layout>
+  </div>
+</template>
+
+<script>
+  import Layout from '@c/Layout.vue'
+export default {
+  name: 'App',
+  components:{Layout}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+  html,body{
+    padding:0;
+    margin:0;
+  }
+</style>
---
+++ src/components/Layout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="layout">
+    <div>Header</div>
+    <slot></slot>
+    <div class="footer">
+      Footer
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Layout',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.layout{
+  /*background:grey;*/
+}
+</style>
